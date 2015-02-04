1bf28eac6a3873c09dd4b0db43e189430473b58b
Oguzhan Topgul <oguzhantopgul@gmail.com>
2015-02-04 15:02:44 +0200

    It is now working on Linux
    Tested on Ubuntu 14.04

--- make.sh
+++ make.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
-flags=()
+if [[ `uname` == 'Darwin' ]]; then
+	flags=()
 
-sdk=/Developer/SDKs/MacOSX10.4u.sdk
-if [[ -e $sdk ]]; then
-    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
+	sdk=/Developer/SDKs/MacOSX10.5.sdk
+	if [[ -e $sdk ]]; then
+	    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
+	fi
+
+	g++ -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+elif [[ `uname` == 'Linux' ]]; then
+	g++ -pipe -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
 fi
-
-for arch in i386 x86_64 ppc armv6; do
-    if g++ -arch "${arch}" --version &>/dev/null; then
-        flags+=(-arch "${arch}")
-    fi
-done
-
-set -x
-g++ "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
